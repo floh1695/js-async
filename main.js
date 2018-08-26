729f19e6da8fbef1d6c7045c8c84e1e38caed96c
Charles Bayley <cbayley95@gmail.com>
2018-08-25 22:49:39 -0400

    Prints out a list of names

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const requestPromise = require('request-promise');
-
-const baseUrl = 'https://swapi.co/api';
-
-requestPromise(`${baseUrl}/people`)
-  .then(response => {
-    console.log(response);
-  });
+const fp = require('lodash/fp');
+const http = require('request-promise-json');
 
 console.log('js-async');
+
+const baseUrl = 'https://swapi.co/api';
+http.get(`${baseUrl}/people`)
+  .then(response => response.results)
+  .then(fp.map('name'))
+  .then(fp.each(console.log));
 
