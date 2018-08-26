5a9fcf919612ea5d93c657105f372aa6c67760dc
Charles Bayley <cbayley95@gmail.com>
2018-08-25 22:04:09 -0400

    Simple request

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 'use strict';
 
+const requestPromise = require('request-promise');
+
+const baseUrl = 'https://swapi.co/api';
+
+requestPromise(`${baseUrl}/people`)
+  .then(response => {
+    console.log(response);
+  });
+
 console.log('js-async');
 
