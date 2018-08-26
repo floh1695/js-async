6e517389fb5bd47a77f7b959f480c1430fa25b49
Charles Bayley <cbayley95@gmail.com>
2018-08-25 23:41:27 -0400

    getAllPages

--- main.js
+++ main.js
@@ -6,8 +6,34 @@ const http = require('request-promise-json');
 console.log('js-async');
 
 const baseUrl = 'https://swapi.co/api';
-http.get(`${baseUrl}/people`)
-  .then(response => response.results)
+
+const getPage = page => http
+  .get(`${baseUrl}/people?page=${page}`);
+
+const getPageResults = page => getPage(page)
+  .then(response => response.results);
+
+const getFirstPage = () => getPageResults(1)
   .then(fp.map('name'))
   .then(fp.each(console.log));
+
+const getAllPages = () => { 
+  const getPageWork = pageNumber => getPage(pageNumber)
+    .then(page => {
+      fp(page.results)
+        .map('name')
+        .each(console.log);
+    
+      const isNextPage = !fp.isNil(page.next);
+      return isNextPage
+        ? getPageWork(pageNumber + 1)
+        : null;
+    });
+  
+  return getPageWork(1);
+};
+
+
+getAllPages();
+
 
